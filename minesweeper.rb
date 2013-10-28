c4bcbd2deef0f099c42775086235e965d3c6da56
App Academy Student <student@appacademy.io>
2013-10-28 16:39:15 -0400

    Finish basic Minesweeper

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,17 +1,15 @@
 require "debugger"
 require "./square"
-require "./player"
 require "./prompt"
 
 class Minesweeper
 
   def initialize
     @board = nil
+    @game_over = false
   end
 
-  def play
-    print "Enter your name: "
-    name = gets.chomp
+  def get_board
 
     print "Enter board size: small (9x9) or medium (16x16): "
     board_size = gets.chomp.downcase
@@ -22,36 +20,139 @@ class Minesweeper
     when "medium"
       @board = make_board(16, 16, 40)
     else
-      puts "Invalid choice, setting to small. "
+      puts "Invalid choice, setting to small!"
       @board = make_board(9, 9, 10)
     end
+  end
 
-    game_over = false
-    until game_over
-      prompt_msg = "Enter row and col (e.g. 0 2): "
-      error_msg = "Invalid position\n"
-      pos_str = prompt(prompt_msg, error_msg) do |input|
-        # Is separated by a space
-        is_valid = input.split(' ').count == 2
+  def get_command
+    # Prompt for position
+    prompt_msg = "Enter command to reveal(r)/flag(f) row and col (e.g. r 0 2): "
+    error_msg = "Invalid input\n"
+    prompt(prompt_msg, error_msg) do |input|
+      # Is separated by spaces
+      is_valid = input.split(' ').count == 3
 
-        # Is in range
-        row, col = input.split(' ')
-        is_valid &&= row.to_i.between?(0, @board.length-1)
-        is_valid &&= col.to_i.between?(0, @board[0].length-1)
-      end
 
-      row, col = pos_str.split(' ').map(&:to_i)
+      cmd, row, col = input.split(' ')
+      # Is a valid command
+      is_valid &&= cmd.downcase == 'r' || cmd.downcase == 'f'
+
+      # Is in range
+      is_valid &&= row.to_i.between?(0, @board.length - 1)
+      is_valid &&= col.to_i.between?(0, @board[0].length - 1)
+
+
+      is_valid = true if input == "quit"
+    end
+  end
+
+  def play
+    print "Enter your name: "
+    name = gets.chomp
+
+    get_board
+
+    display_board
+
+    until @game_over
+      command_str = get_command
+      return if command_str == "quit"
+
+      cmd, row, col = command_str.split(' ')
+      row = row.to_i
+      col = col.to_i
+
+      update_board(cmd, row, col)
+
+      display_board
+
+      check_win
+    end
+
+    puts "Game Over!"
+
+    # Show revealed
+    display_board(true)
+
+  end
+
+  def check_win
+    won = true
+
+    num_flagged = 0
+    num_mines = 0
+    @board.flatten.each do |square|
+      won &&= (square.is_revealed || square.is_flagged)
+      num_flagged += 1 if square.is_flagged
+      num_mines += 1 if square.has_mine
+    end
+
+    won &&= (num_mines == num_flagged)
+
+    if won
+      puts "You won!"
+      @game_over = true
     end
 
   end
 
-  def display_board
-    @board.each do |row|
+  def update_board(cmd, row, col)
+    square = @board[row][col]
+    @game_over = true if cmd == 'r' && square.has_mine
+
+    square.update(cmd)
+  end
+
+  def display_board(reveal_all = false)
+    if reveal_all
+      # Show revealed
+      @board.flatten.each do |square|
+        square.is_revealed = true
+      end
+    end
+
+    puts
+
+    # Display row header
+    puts "  " + (0..@board[0].length-1).to_a.map(&:to_s).join(' ')
+    puts "  " + "--" * @board[0].length
+
+    # Display board
+    @board.each_with_index do |row, r|
+      print "#{r}|" # Display column header
       row.each do |square|
-        print square.display_symbol
+        print square.display_symbol + " " # Print square
       end
       puts
     end
+
+    puts
+  end
+
+  def make_board_with_mines(board, mines)
+    # Generate random mine positions
+    mine_positions = []
+    until mine_positions.length == mines
+      row = rand(board.length)
+      col = rand(board[0].length)
+      mine_positions << [row, col] if !mine_positions.include?([row, col])
+    end
+
+    # For each mine
+    mine_positions.each do |pos|
+      row, col = pos
+      # Add mine to board
+      board[row][col].has_mine = true
+
+      # Increment number of adjacent mines counter on adjacent squares
+
+      board[row][col].adjacent_squares.each do |adj_square|
+       adj_square.adjacent_mines += 1
+      end
+    end
+
+    board
   end
 
   def make_board(rows, cols, mines)
@@ -68,26 +169,7 @@ class Minesweeper
       end
     end
 
-    # Generate random mine positions
-    mine_positions = []
-    until mine_positions.length == mines
-      row = rand(rows)
-      col = rand(cols)
-      mine_positions << [row, col] if !mine_positions.include?([row, col])
-    end
-
-    # For each mine
-    mine_positions.each do |pos|
-      row, col = pos
-      # Add mine to board
-      board[row][col].has_mine = true
-
-      # Increment number of adjacent mines counter on adjacent squares
-
-      board[row][col].adjacent_squares.each do |adj_square|
-       adj_square.adjacent_mines += 1
-      end
-    end
+    board = make_board_with_mines(board, mines)
 
     return board
   end
--- square.rb
+++ square.rb
@@ -1,19 +1,19 @@
 class Square
-  attr_accessor :adjacent_mines, :is_flagged, :has_mine, :is_unexplored,
+  attr_accessor :adjacent_mines, :is_flagged, :has_mine, :is_revealed,
                 :adjacent_squares
 
   def initialize
     @adjacent_mines = 0
     @is_flagged = false
     @has_mine = false
-    @is_unexplored = true
+    @is_revealed = false
     @adjacent_squares = []
   end
 
   def display_symbol
     if @is_flagged # Flagged
       return 'F'
-    elsif @is_unexplored # Not flagged, unexplored
+    elsif !@is_revealed # Not flagged, unexplored
       return '?'
     elsif @has_mine # Not flagged, explored, has_mine (Game over!)
       return '*'
@@ -23,6 +23,27 @@ class Square
       else
         return @adjacent_mines.to_s # Not flagged, explored, near >= 1 mines
       end
+    end
+  end
+
+  def update(cmd)
+    if cmd == 'f' # Flag
+      self.is_flagged = !self.is_flagged
+    else # Reveal
+      unless self.is_flagged
+        self.is_revealed = true
+        self.reveal_blanks([])
+      end
+    end
+  end
+
+  def reveal_blanks(blank_squares)
+    return unless @adjacent_mines == 0 || @is_flagged
+
+    blank_squares += [self]
+    @adjacent_squares.each do |adj_square|
+      adj_square.is_revealed = true
+      adj_square.reveal_blanks(blank_squares) unless blank_squares.include?(adj_square)
     end
   end
 end
